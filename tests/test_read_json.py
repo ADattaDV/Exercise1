965fbdde89650e6cf87f232459749e87e6742398
Aakash Datta <ad6g18@soton.ac.uk>
2021-07-01 13:40:34 +0000

    fixed json read test

--- tests/test_read_json.py
+++
@@ -1,7 +0,0 @@
-from exercise1 import *
-import pytest
-
-def test_json_reader_is_dict():
-    file_name = './run_results.json'
-    file_output = read_file('run_results.json')
-    assert isinstance(file_output, dict)
