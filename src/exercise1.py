5a277a5c445c2e7f47c01c6f42676830a1aa57c2
Aakash Datta <ad6g18@soton.ac.uk>
2021-07-01 14:17:15 +0000

    Outputs tests as individual json files, stores which tests were passed

--- src/exercise1.py
+++ src/exercise1.py
@@ -4,20 +4,27 @@ import os
 
 
 def read_file(file_name):
-    with open(os.path.join(sys.path[0], file_name)) as file:
+    path = '/home/aakashdatta/PycharmProjects/Exercise1/src'
+    with open(os.path.join(path, file_name)) as file:
         data = json.load(file)
+        file.close
     return data
 
 
 results_data = read_file('run_results.json')
-suc_data = []
-for test in results_data:
-    print(test)
-print(results_data.keys())
 
-for test in results_data['results']:
-    if test['status'] == 'pass':
-        suc_data.append(test)
-
-print(suc_data)
-print("hi")
+def does_pass_test:
+    suc_data = []
+    fail_data = []
+    pass_count = 0
+    for index, test in enumerate(results_data['results']):
+        test_f_name = 'test_num{0}.json'.format(index)
+        with open(test_f_name, 'w') as json_test:
+            json.dump(test, json_test)
+            json_test.close()
+        if test['status'] == 'pass':
+            suc_data.append([index, test])
+            pass_count += 1
+        elif test['status'] == 'fail':
+            fail_data.append([index, test])
+print(fail_data)
